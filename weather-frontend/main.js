1ea45835ac2ebc363f8adfc0f618213b41ced67f
Constantin Pusch <puschc@tcd.ie>
2023-10-25 17:48:46 -0400

    attempted to add charts

--- weather-frontend/main.js
+++ weather-frontend/main.js
@@ -5,10 +5,17 @@ new Vue({
     loaded: false,
     weatherData: [],
     city: "", // Store the city entered by the user
+    temperatureData: [{ timestamp: 1635613200, value: 22 }],
+    chartDataLoaded: false,
+    tempChart: null,
+    windChart: null,
+    rainChart: null,
+    pollutionChart: null,
   },
   methods: {
     searchWeather() {
       this.loaded = false;
+      this.chartDataLoaded = false;
       this.loading = true;
       // Construct the API URL with the user's entered city
       const apiUrl = `http://127.0.0.1:3000/weather/${this.city}`;
@@ -17,6 +24,11 @@ new Vue({
         .get(apiUrl)
         .then((response) => {
           this.weatherData = response.data;
+          this.chartDataLoaded = true;
+          this.createChart("tempChart", this.temperatureData);
+          this.createChart("windChart", this.temperatureData);
+          this.createChart("rainChart", this.temperatureData);
+          this.createChart("pollutionChart", this.temperatureData);
         })
         .catch((error) => {
           console.error("Error fetching weather data:", error);
@@ -25,6 +37,49 @@ new Vue({
           this.loading = false;
           this.loaded = true;
         });
+    },
+    createChart(containerId, data) {
+      const canvas = document.querySelector(`.${containerId}`);
+
+      if (this[containerId]) {
+        // Destroy the existing chart if it exists
+        this[containerId].destroy();
+      }
+
+      if (canvas) {
+        const ctx = canvas.getContext("2d");
+
+        this[containerId] = new Chart(ctx, {
+          type: "line",
+          data: {
+            labels: data.map((point) => point.timestamp),
+            datasets: [
+              {
+                label: "Data",
+                data: data.map((point) => point.value),
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1,
+                fill: false,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              x: {
+                type: "time",
+                time: {
+                  unit: "hour",
+                },
+              },
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
+      }
     },
   },
 });
